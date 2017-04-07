42c03dc985e67db1b6bdf219af70f36bcccae7cb
Jing Huo <huoj@andrew.cmu.edu>
2017-04-07 14:30:22 -0400

    use yahoo weather api to retrieve weather data (location hardcoded)

--- Bringo/app/src/main/java/com/example/bringo/SettingsActivity.java
+++ Bringo/app/src/main/java/com/example/bringo/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.example.bringo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.bringo.supportingapis.YahooWeather;
+
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.Manifest.permission.ACCESS_NOTIFICATION_POLICY;
@@ -14,5 +16,9 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        YahooWeather weather = new YahooWeather();
+        weather.refreshWeather("pittsburgh");
+        System.out.println("the weather is: " + weather.getWeather("07 Apr 2017"));
     }
 }
