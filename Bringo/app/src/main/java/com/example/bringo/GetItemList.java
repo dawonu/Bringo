cf010e1b74857fe295d680a1796bd2008af6d92c
holso66 <yidix@andrew.cmu.edu>
2017-04-20 14:07:00 -0400

    update from Sophie 04/20

---
+++ Bringo/app/src/main/java/com/example/bringo/GetItemList.java
@@ -0,0 +1,8 @@
+package com.example.bringo;
+
+/**
+ * Created by xuyidi on 4/19/17.
+ */
+
+public class GetItemList {
+}
---
+++ Bringo/app/src/main/java/com/example/bringo/database/CheckedItemsDB.java
@@ -0,0 +1,8 @@
+package com.example.bringo.database;
+
+/**
+ * Created by xuyidi on 4/19/17.
+ */
+
+public class CheckedItemsDB {
+}
